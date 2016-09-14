98d784ee1533141f49e0b90aac780457bf1aca70
Adam Tse <mr.adam.tse@gmail.com>
2016-09-14 15:44:35 -0400

    added js X and Os

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $(function(){
-  alert('HELLO FIRNED');
+  var count = 0;
+  $('.square').on('click', function(){
+    var call = $(this);
+    if ( count % 2 === 0 ){
+      call.text("X");
+    } else {
+      call.text("O");
+    };
+    count++;
+  })
 })
